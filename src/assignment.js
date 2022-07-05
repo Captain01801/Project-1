87e2a4e874ae6699720e59dd59f2806b203cf002
Akinsulere Olajobi <55103437+Captain01801@users.noreply.github.com>
2022-07-05 08:32:24 +0000

    Simply add functionalities to do the calculation, and i uncommented the lines at the end of the two tasks, to give out my final output

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+    let total = 0;
+    for (let count = 0; count < arrayOfNumbers.length; count++) {
+      total = total + arrayOfNumbers[count]
+    }
+    return total;
+} 
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumbers = 0;
+    for (let count = 0; count < arrayOfNumbers.length; count++) {
+      if (arrayOfNumbers[count] % 2 == 0) {
+        evenNumbers++
+      }
+    }
+    return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
